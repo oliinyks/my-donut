7ac47dfb882f3e10d43267ab315f9eeef1ffa79a
BigBowse <32816486+BigBowse@users.noreply.github.com>
2022-06-01 11:05:27 +0000

    Deploying to gh-pages from @ oliinyks/my-donut@319e58c16ee77a5fb0ddaca34e36e6a687841e23 🚀

---
+++ index.e66dbf72.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[mobile-menu-open]"),closeModalBtn:document.querySelector("[mobile-menu-close]"),modal:document.querySelector("[mobile-menu]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.e66dbf72.js.map
