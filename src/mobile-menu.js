ec12b67ff55fd8477e103316ef629baf45989fc0
Olijnyks <olijnyks@ukr.net>
2022-06-09 12:06:27 +0300

    Update mobile-menu.js

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -1,14 +1,32 @@
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[mobile-menu-open]"),
+//     closeModalBtn: document.querySelector("[mobile-menu-close]"),
+//     modal: document.querySelector("[mobile-menu]"),
+//   };
+
+//   refs.openModalBtn.addEventListener("click", toggleModal);
+//   refs.closeModalBtn.addEventListener("click", toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+// })();
+
 (() => {
-  const refs = {
-    openModalBtn: document.querySelector("[mobile-menu-open]"),
-    closeModalBtn: document.querySelector("[mobile-menu-close]"),
-    modal: document.querySelector("[mobile-menu]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
+ const refs = {
+  openModalBtn: document.querySelector('[mobile-menu-open]'),
+  closeModalBtn: document.querySelector('[mobile-menu-close]'),
+  modal: document.querySelector('[mobile-menu]'),
+  menuList: document.querySelector('.mobile-menu-nav___list'),
+ };
+ refs.openModalBtn.addEventListener('click', toggleModal);
+ refs.closeModalBtn.addEventListener('click', toggleModal);
+ refs.menuList.addEventListener('click', removeModal);
+ function toggleModal() {
+  refs.modal.classList.toggle('is-hidden');
+ }
+ function removeModal() {
+  refs.modal.classList.add('is-hidden');
+ }
 })();
